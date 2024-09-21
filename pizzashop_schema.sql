27870d1cb4c72514dd8ceb3d0e3a53ffcdb3c189
mariowettig <m.wettig@student.maastrichtuniversity.nl>
2024-09-21 22:07:58 +0200

    its a me

---
+++ pizzashop_schema.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE IF NOT EXISTS pizza_shop;
+USE pizza_shop;
+
+DROP TABLE IF EXISTS Pizza;
+
+CREATE TABLE Pizza (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL(5,3) NOT NULL,
+    vegetarian BOOLEAN DEFAULT FALSE,
+    vegan BOOLEAN DEFAULT FALSE
+);
+
+DROP TABLE IF EXISTS Ingredients;
+
+CREATE TABLE Ingredients (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    cost DECIMAL(5, 2) NOT NULL
+    vegetarian BOOLEAN
+    vegan BOOLEAN
+);
+
+DROP TABLE IF EXISTS Pizza_Ingredients;
+
+CREATE TABLE Pizza_Ingredients (
+    pizza_id INT,
+    ingredient_id INT,
+    FOREIGN KEY (pizza_id) REFERENCES Pizza(id),
+    FOREIGN KEY (ingredient_id) REFERENCES Ingredients(id),
+    PRIMARY KEY (pizza_id, ingredient_id)
+);
